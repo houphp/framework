ec46c5f74373ab9ac25e6fa5415fc7f8530a41c6
houphp <2869571987@qq.com>
2019-08-24 15:20:08 +0800

    base

--- Common/Terminal.php
+++ Common/Terminal.php
@@ -1,13 +1,14 @@
 <?php
 namespace houphp\Common;
-
-/**
- * Terminal
- *
- * @package Core
- *
- * @author Zorin Vasily <kak.serpom.po.yaitsam@gmail.com>
- */
+// +----------------------------------------------------------------------
+// | HOUCMS [ 厚匠科技 专注建站 APP PC 微站 小程序 WAP ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2017 http://www.houphp.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Amos <447107108@qq.com> http://www.houjit.com
+// +----------------------------------------------------------------------
 class Terminal
 {
     /*
